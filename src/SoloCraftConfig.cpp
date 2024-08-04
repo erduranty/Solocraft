eb945f97b3c4681e677118617a9f785aa0b1b7ec
erduranty <32974117+erduranty@users.noreply.github.com>
2024-08-03 22:36:26 -0400

    Update SoloCraftConfig.cpp

--- src/SoloCraftConfig.cpp
+++ src/SoloCraftConfig.cpp
@@ -7,7 +7,7 @@
 
 namespace cmangos_module
 {
-    SolocraftConfig::SolocraftConfig(): ModuleConfig("SoloCraft.conf") , enabled(false) , cost(0U)
+    SolocraftConfig::SolocraftConfig(): ModuleConfig("SoloCraft.conf") , enabled(false)
 
         bool SolocraftConfig::Initialize()
         {
